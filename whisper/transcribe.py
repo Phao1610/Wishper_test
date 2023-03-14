6b51c3d91e97992499fb375f2b00b829d16932e3
Phao1610 <nguyentoan16102002@gmail.com>
2023-03-14 09:22:44 +0700

    push

--- whisper/transcribe.py
+++ whisper/transcribe.py
@@ -107,6 +107,7 @@ def transcribe(
     A dictionary containing the resulting text ("text") and segment-level details ("segments"), and
     the spoken language ("language"), which is detected when `decode_options["language"]` is None.
     """
+    print(decode_options)
     dtype = torch.float16 if decode_options.get("fp16", True) else torch.float32
     if model.device == torch.device("cpu"):
         if torch.cuda.is_available():
